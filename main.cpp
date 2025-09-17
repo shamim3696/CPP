8b30a0ac311459f206b166da2cc33723ec52ccc7
Shamim <shamim36960@gmail.com>
2025-09-17 17:02:14 +0600

    First c++ code

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "My age is: " << age << endl;
+    return 0;
+}

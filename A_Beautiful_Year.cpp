8ad42a237cbd9856668bbd2f56dac98d1bb8032d
shamim3696 <shamim36960@gmail.com>
2025-10-07 23:28:10 +0600

    A beautiful problem solved

--- A_Beautiful_Year.cpp
+++ A_Beautiful_Year.cpp
@@ -1,13 +1,26 @@
 #include<bits/stdc++.h>
-uisng namespace std;
 
-int main(){
+using namespace std;
 
-    int y;
+int main() {
+    int year;
+    cin >> year;
 
-    cin >> y;
+    while(1){
+        year++;
+        string year_str = to_string(year);
 
-    
+        set<char> year_set(year_str.begin(), year_str.end());
+
+        int len1 = year_str.length();
+        int len2 = year_set.size();
+
+        if(len1 == len2){
+            cout << year << endl;
+            break;
+        }
+
+    }
 
     return 0;
 }

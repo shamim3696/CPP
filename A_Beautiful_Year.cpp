c90b4b9da759c104917c1765e01e7ee612606d0b
Shamim <shamim36960@gmail.com>
2025-11-17 13:29:50 +0600

    feat: add leap year check to determine correct date

--- A_Beautiful_Year.cpp
+++ A_Beautiful_Year.cpp
@@ -5,7 +5,14 @@ int main(){
 
     int y;
 
-    cin >> y;
+    cin >> y; // year
+
+    if(y%4==0 && y%100!=0 || y%400==0){
+        cout << "12.09." << y << endl;
+    }
+    else{
+        cout << "13.09." << y << endl;
+    }
 
     
 

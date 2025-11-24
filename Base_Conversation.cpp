0682e23a175e9c526a96a6536832ed559fee9a82
Shamim <shamim36960@gmail.com>
2025-11-24 14:43:33 +0600

    feat: add Base_Conversation.cpp to convert numbers from any base to decimal

---
+++ Base_Conversation.cpp
@@ -0,0 +1,27 @@
+// Problem statement: First I will take input for Base then an integer N. I have to convert it to its equivalent value in decimal.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int B,N;
+    cout << "Enter two integers: "<<endl;;
+    cin>>B>>N;
+
+    int ans = 0;
+    int power = 0;
+    
+    while(N>0){
+        int rem = N % 10;
+        N = N / 10;
+        double powValue = pow(B, power);
+        int value = rem * powValue;
+        power++;
+        ans += value;
+
+    }
+    cout << "Decimal value is: "<<ans<<endl;
+
+    return 0;
+}
